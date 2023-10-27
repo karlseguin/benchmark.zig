6aebbba30a6cb24fed5720db1a86990cdb7e6b7f
Karl Seguin <k@openmymind.io>
2023-10-27 21:04:23 +0800

    add standard target & optimization to build

--- build.zig
+++ build.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
+	_ = b.standardTargetOptions(.{});
+	_ = b.standardOptimizeOption(.{});
+
 	// Expose this as a module that others can import
 	_ = b.addModule("benchmark", .{
 		.source_file = .{ .path = "benchmark.zig" },
